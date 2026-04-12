08f5022d6f978266a8cd16ebdf413e140039b858
靳晓波 <b357886376@163.com>
2026-04-12 17:27:58 +0800

    first commit

---
+++ src/test/java/com/develop/infrastructure/DevelopInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopInfrastructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
